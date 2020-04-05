4f6aa23e8c910368c8b4daa48bb8858ce9db1339
Thelma Boamah <thelma.a.boamah@gmail.com>
2020-04-05 07:08:43 -0700

    done w/ Table for now

--- src/Table.java
+++ src/Table.java
@@ -1,44 +1,131 @@
+/**
+ * Table is where the initial game state is set up and subsequently tracked during game play.
+ */
 public class Table {
-int width;
-int height;
-Ball ball;
-LeftPaddle leftPaddle;
-RightPaddle rightPaddle;
-//Score leftPaddleScore;
-//Score rightPaddleScore;
+
+   private int difficulty;
+   private int mode;
+
+   private int leftScore;
+   private int rightScore;
+
+   private Ball ball;
+   private LeftPaddle leftPaddle;
+   private RightPaddle rightPaddle;
 
     /**
      * Table constructor
-     * @param width
-     * @param height
-     * @param ball
-     * @param lp
-     * @param rp
+     * @param difficulty
+     * @param mode
      */
-    public Table(int width, int height, Ball ball, LeftPaddle lp, RightPaddle rp) {
-        this.width = width;
-        this.height = height;
-        this.ball = ball;
-        this.leftPaddle  = lp;
-        this.rightPaddle = rp;
-//        this.leftPaddleScore = new Score();
-//        this.rightPaddleScore = new Score();
+    public Table(int difficulty, int mode) {
+        this.difficulty = difficulty;
+        this.mode = mode;
+        leftScore = 0;
+        rightScore = 0;
+        ball = getBallByLevel();
+        leftPaddle = getLeftPaddleByLevel();
+        rightPaddle = getRightPaddleByLevel();
+
+        createTable();
+        initGame();
+
+        // TODO: Incorporate Obstacles
+
     }
 
     /**
-     * Create a table using PennDraw
+     * Get Ball class based on level (difficulty and mode)
+     * @return Ball
      */
-    public void createTable() {
-        PennDraw.setCanvasSize(width, height);
-        PennDraw.setScale(0, height);
+    private Ball getBallByLevel() {
+        Ball ball;
+
+        // difficulty is 1, 2, or 3; mode is 1 or 2
+        if (difficulty == 1 && mode == 1) {
+            // make me ball
+        } else if (difficulty == 1 && mode == 2) {
+            // ...
+        } else if (difficulty == 2 && mode == 1) {
+            // ...
+        } else if (difficulty == 2 && mode == 2) {
+            // ...
+        } else if (difficulty == 3 && mode == 1) {
+            // ...
+        } else {
+            // ... (diff 3 and mode 2 case)
+        }
+
+        // return ball, below if to prevent compile error
+        return new Ball(250, 250, 25);
+    }
+
+    /**
+     * Get LeftPaddle class based on level
+     * @return LeftPaddle
+     */
+    private LeftPaddle getLeftPaddleByLevel() {
+        LeftPaddle lp;
+
+        // difficulty is 1, 2, or 3; mode is 1 or 2
+        if (difficulty == 1 && mode == 1) {
+            // make me ball
+        } else if (difficulty == 1 && mode == 2) {
+            // ...
+        } else if (difficulty == 2 && mode == 1) {
+            // ...
+        } else if (difficulty == 2 && mode == 2) {
+            // ...
+        } else if (difficulty == 3 && mode == 1) {
+            // ...
+        } else {
+            // ... (diff 3 and mode 2 case)
+        }
+
+        // return lp, below if to prevent compile error
+        return new LeftPaddle(10, 250, 10);
+    }
+
+    /**
+     * Get RightPaddle class based on level
+     * @return RightPaddle
+     */
+    private RightPaddle getRightPaddleByLevel() {
+        RightPaddle rp;
+
+        // difficulty is 1, 2, or 3; mode is 1 or 2
+        if (difficulty == 1 && mode == 1) {
+            // make me ball
+        } else if (difficulty == 1 && mode == 2) {
+            // ...
+        } else if (difficulty == 2 && mode == 1) {
+            // ...
+        } else if (difficulty == 2 && mode == 2) {
+            // ...
+        } else if (difficulty == 3 && mode == 1) {
+            // ...
+        } else {
+            // ... (diff 3 and mode 2 case)
+        }
+
+        // return rp, below if to prevent compile error
+        return new RightPaddle(490, 250, 10);
+    }
+
+
+    /**
+     * Use PennDraw to create Table frame
+     */
+    private void createTable() {
+        PennDraw.setCanvasSize(Pong.WIDTH, Pong.HEIGHT);
+        PennDraw.setScale(0, Pong.HEIGHT);
         PennDraw.enableAnimation(60);
     }
 
     /**
-     * Set up initial game state
+     * Launch game UI with initial state
      */
-    public void initGame() {
-        createTable();
+    private void initGame() {
 
         while(true) {
             PennDraw.clear();
@@ -61,23 +148,23 @@ RightPaddle rightPaddle;
     /**
      * Increment and decrement left and right paddle scores during game play
      */
-    public void adjustScore() {
+    private void updateScore() {
+        // Detect where the ball goes outside of table and allocate point to the right side
+        // At different levels, there may be conditions that cause player to lose points
+        // calls writeScore()
+    }
+
+    /**
+     * Write the current score to the table frame
+     */
+    private void writeScore() {
 
     }
 
     /**
-     * Get the left paddle score
-     * @return int
+     * Reset the ball and paddle positions after a player scores
      */
-//    public int getLeftPaddleScore() {
-//        return leftPaddleScore.currentScore();
-//    }
+    private void reset() {
 
-    /**
-     * Get the right paddle score
-     * @return int
-     */
-//    public int getRightPaddleScore() {
-//        return rightPaddleScore.currentScore();
-//    }
+    }
 }
