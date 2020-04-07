6ee95aab495ec5ab318e1198b203944701e471f3
Thelma Boamah <thelma.a.boamah@gmail.com>
2020-04-06 21:24:45 -0700

    test case for Vector2 times method

---
+++ src/Vector2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Vector2Test {
+
+    @Test
+    public void times() {
+
+        Vector2 v2 = new Vector2(5,10);
+        Vector2 doubleV2 = v2.times(2);
+
+        assertEquals(doubleV2.x, 10);
+        assertEquals(doubleV2.y, 20);
+    }
+}
