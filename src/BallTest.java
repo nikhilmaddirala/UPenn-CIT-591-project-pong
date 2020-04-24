b6792a808ccca7ff5577f1ce1040c8174c8787b3
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2020-04-24 13:59:49 -0700

    remove test

---
+++ src/BallTest.java
@@ -0,0 +1,13 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.Test;
+
+// public class BallTest {
+    
+//     @Test
+//     public void getBallSpeed(){
+//         Ball myBall = new Ball();
+//         assertEquals(20, myBall.getBallSpeed(), message);
+//     }
+
+// }
