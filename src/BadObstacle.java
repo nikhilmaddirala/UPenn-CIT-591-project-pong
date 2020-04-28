d43e2cd94cd752dc603b2ead4710c2cb4247a4ad
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2020-04-27 17:34:55 -0700

    remove empty method

--- src/BadObstacle.java
+++ src/BadObstacle.java
@@ -7,13 +7,4 @@ public class BadObstacle extends Obstacle {
 		super(x, y);
 	}
 
-	/**
-	 * This method triggers a new ball based on the result of checkForTrigger.
-	 * 
-	 * @param checkForTrigger
-	 */
-	public void triggerEvent(boolean checkForTrigger) {
-
-	}
-
 }
